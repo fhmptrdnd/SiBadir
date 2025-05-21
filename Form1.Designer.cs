35a74091d3946e05edba0cfdd817c78ccfc11024
FAHMI PUTRA <fahmiputradendi@gmail.com>
2025-05-21 11:17:42 +0700

    test doang ini mah

---
+++ Form1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace SiBadir
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(1331, 703);
+            Name = "Form1";
+            Text = "Si Badir";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
